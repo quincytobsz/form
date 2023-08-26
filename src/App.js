ba20311a30c5103a67b87c346cb95a3d064457cb
quincytobsz <quincytobayiwa@gmail.com>
2023-08-26 21:25:51 +0200

    Table

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { useState } from 'react';
-import './App.css';
-import React from 'react';
-import Table from './components/Table';
-import Data from './components/Data';
-
-
-
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+import Table from "./components/Table";
+import Data from "./components/Data";
 
 
 function App() {
-  const {fieldOpen,setFieldOpen}= useState{false};
+  const {fieldOpen, setFieldOpen}= useState(false);
   return(
     <div>
       <Table/>
-      <button className="btn">Add User</button>
-    {fieldOpen && <Data/>}
+      <button className="btn" onClick={()=>setFieldOpen(true)}>
+        Add User</button>
+    {fieldOpen && <Data closeField={()=>setFieldOpen(false)}/>}
     </div>
 );
 }
